560bd695ba33f12c20521db1a476cbfb15a4c2c5
timbyxty <timbyxtyt@gmail.com>
2022-09-10 11:09:08 +0300

    delete hanlder test

--- handlers/delete_handler.py
+++ handlers/delete_handler.py
@@ -1,8 +1,14 @@
 from handlers.base_handler import BaseHandler
 import json
+from db_utils import db
 
 
 class DeletesHandler(BaseHandler):
     async def delete(self, id):
-        print(id)
-        self.write("Tima lox")
+        answer: dict = dict()
+        date = self.get_query_argument("date", None)
+        await db.delete(id, date)
+        answer["code"] = 200
+        answer["message"] = "123ZXC"
+        self.set_status(answer["code"])
+        self.write(json.dumps(answer))
